1de32f3be127b80fa5d384f537309d1c93432978
gtowne <gtowne@rbmtechnologies.com>
2017-08-19 22:03:42 -0400

    Update routes and add TwitterEntities controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/:provider/callback', to: 'sessions#create'
+
+  resources :twitter_entities
+
+  root to: "twitter_entities#index"
 end
